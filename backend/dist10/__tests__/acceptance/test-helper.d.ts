83a46e9d209e94979a213427db4e2752a664493d
Guillaume Lagorce <glagorce@octo.com>
2019-04-08 19:20:23 +0200

    [skip ci] Add nginx configuration

--- backend/dist10/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { BackendApplication } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: BackendApplication;
-    client: Client;
-}
