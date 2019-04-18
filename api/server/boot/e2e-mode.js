cfb47168cf86750464ddead00fad5cedb5179334
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-04-18 14:27:01 +0200

    [CI] Avoid using `source` as CircleCI does not support it

--- api/server/boot/e2e-mode.js
+++ api/server/boot/e2e-mode.js
@@ -2,6 +2,7 @@ const poleEmploiApiMock = require('../../tests/pole-emploi-api-mock')
 
 module.exports = () => {
   if (process.env.NODE_ENV === 'e2e') {
+    console.log('E2E mode activated!')
     poleEmploiApiMock.set()
   }
 }
