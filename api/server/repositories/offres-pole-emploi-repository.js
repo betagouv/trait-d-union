0e023cd16d834508c7950ddad2e5c252d126af6d
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-06 09:01:25 +0200

    [Offres] Refactor searching offres on PE repository

--- api/server/repositories/offres-pole-emploi-repository.js
+++ api/server/repositories/offres-pole-emploi-repository.js
@@ -1,27 +1,34 @@
-module.exports = ({ poleEmploiApiService }) => ({
-  getOffres: async ({ codeROME }) => {
-    const searchParameters = {
-      codeROME,
-      commune: 57463,
-      distance: 10,
-      experience: 1,
-      range: '1-149',
-      sort: 2
+module.exports = ({ poleEmploiApiService }) => {
+  return {
+    getOffres: async ({ codeROME }) => {
+      const offres = await searchOffres(poleEmploiApiService)(codeROME)
+      return offres.map(sanitizeOffre)
     }
-    const offres = await poleEmploiApiService.request('/offres/search', searchParameters)
-    return offres.resultats.map(sanitizeOffre)
   }
-})
+}
+
+const searchOffres = (poleEmploiApiService) => async (codeROME) => {
+  const searchParameters = {
+    codeROME,
+    commune: 57463,
+    distance: 10,
+    experience: 1,
+    range: '1-149',
+    sort: 2
+  }
+  const { resultats } = await poleEmploiApiService.request('/offres/search', searchParameters)
+  return resultats
+}
 
 function sanitizeOffre (offre) {
-  const properties = ['id', 'description', 'dureeTravailLibelle', 'secteurActiviteLibelle',
-    'lieuTravail', 'appellationlibelle', 'salaire', 'permis', 'natureContrat',
-    'typeContrat']
+  const properties = [
+    'id', 'description', 'dureeTravailLibelle', 'secteurActiviteLibelle', 'lieuTravail',
+    'appellationlibelle', 'salaire', 'permis', 'natureContrat', 'typeContrat'
+  ]
 
   const result = keepDefinedProperties(offre, properties)
 
-  const urlOffre = offre.origineOffre && offre.origineOffre.urlOrigine
-  result.url = urlOffre
+  result.url = offre.origineOffre && offre.origineOffre.urlOrigine
   result.courriel = 'some@email.fr'
 
   return result
