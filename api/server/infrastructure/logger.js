7ff2f704faf276c384c62cb3e541a0c6a9253a98
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-09 11:30:31 +0200

    [Log] Disable log when unit testing

--- api/server/infrastructure/logger.js
+++ api/server/infrastructure/logger.js
@@ -4,6 +4,10 @@ const formatOut = bformat({ outputMode: 'short' })
 
 const logger = bunyan.createLogger({ name: 'traitdunion-api', stream: formatOut, level: 'debug' })
 
+if (process.env.NODE_ENV === 'test') {
+  logger.level(bunyan.FATAL + 1)
+}
+
 const debug = logger.debug.bind(logger)
 const warn = logger.warn.bind(logger)
 const info = logger.info.bind(logger)
