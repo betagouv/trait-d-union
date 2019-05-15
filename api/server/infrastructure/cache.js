8fb1e96c864cff0929a5d7a97440efee590e4e2b
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-15 14:54:47 +0200

    [Cache] Increase cache expiration to 1 day

--- api/server/infrastructure/cache.js
+++ api/server/infrastructure/cache.js
@@ -1,6 +1,6 @@
 const LRU = require('lru-cache')
 
-const maxAge = 60 * 60 * 1000 // 1 hour
+const maxAge = 24 * 60 * 60 * 1000 // 1 day
 const _cache = new LRU({ maxAge })
 
 module.exports = _cache
