0892258a91f6fde68976ec0a3b724cff9a85a60b
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-16 16:39:53 +0200

    [redirect] forward utm_source query parameter

--- api/server/boot/redirect.js
+++ api/server/boot/redirect.js
@@ -1,7 +1,9 @@
 module.exports = (app) => {
   app.get('/', (req, res) => {
+    const utmSource = req.query.utm_source
+    const queryParams = utmSource ? `?utm_source=${utmSource}` : ''
     res.writeHead(302, {
-      'Location': 'http://candidat.traitdunion.beta.gouv.fr'
+      'Location': `http://candidat.traitdunion.beta.gouv.fr${queryParams}`
     })
     res.end()
   })
