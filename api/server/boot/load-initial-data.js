b3c6ab8e14c36b3a32f8227ac7531b5c165e4124
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-16 17:09:38 +0200

    [Boot] Avoid dropping on reboot

--- api/server/boot/load-initial-data.js
+++ api/server/boot/load-initial-data.js
@@ -2,6 +2,6 @@ const loadReferentiels = require('../features/referentiels/load-referentiels')
 const referentiels = require('../features/referentiels')
 
 module.exports = async (app) => {
-  await app.datasources.db.automigrate()
+  await app.datasources.db.autoupdate()
   return loadReferentiels(app.models, referentiels)
 }
