02a2203d0529c39161402ed4eda4761f893e1bc3
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-22 21:58:59 +0200

    [Staging] fix redirect

--- api/server/boot/redirect.js
+++ api/server/boot/redirect.js
@@ -2,13 +2,16 @@ module.exports = (app) => {
   app.get('/', (req, res, next) => {
     const utmSource = req.query.utm_source
     const queryParams = utmSource ? `?utm_source=${utmSource}` : ''
-    const environment = req.hostname.includes('staging') ? 'staging.' : ''
-    if (environment === 'staging') {
+    if (isStaging(req)) {
       return next()
     }
     res.writeHead(302, {
-      'Location': `http://candidat.traitdunion.${environment}beta.gouv.fr${queryParams}`
+      'Location': `http://candidat.traitdunion.beta.gouv.fr${queryParams}`
     })
     res.end()
   })
+}
+
+function isStaging (req) {
+  return req.hostname.includes('staging')
 }
