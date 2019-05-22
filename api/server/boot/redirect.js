9c719e17b0fe82f68e6e801570e719093a457bb2
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-22 17:11:02 +0200

    [Redirect] redirect on staging if coming from staging

--- api/server/boot/redirect.js
+++ api/server/boot/redirect.js
@@ -2,8 +2,9 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     const utmSource = req.query.utm_source
     const queryParams = utmSource ? `?utm_source=${utmSource}` : ''
+    const environment = req.hostname.includes('staging') ? 'staging.' : ''
     res.writeHead(302, {
-      'Location': `http://candidat.traitdunion.beta.gouv.fr${queryParams}`
+      'Location': `http://candidat.traitdunion.${environment}beta.gouv.fr${queryParams}`
     })
     res.end()
   })
