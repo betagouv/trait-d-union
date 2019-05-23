ae0a7a58c27f2df57e3ba7fc4f70dbacfb23d803
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-23 17:32:48 +0200

    [Front] Add link to typeform

--- front/app/components/offres-listing.js
+++ front/app/components/offres-listing.js
@@ -2,5 +2,11 @@ import Component from '@ember/component'
 import fade from 'ember-animated/transitions/fade'
 
 export default Component.extend({
-  transition: fade
+  transition: fade,
+
+  actions: {
+    postuler (offre) {
+      window.open(`https://traitdunion.typeform.com/to/FsNL2J?id_offre=${offre.id}`, '_blank')
+    }
+  }
 })
