ba8d5c9c7ef5880921d7a027f15eb2e22d0c2e45
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-05-28 07:47:44 +0200

    [Offres] Add the niveauQualificationEntree computing

--- api/server/models/action-formation.js
+++ api/server/models/action-formation.js
@@ -1,4 +1,15 @@
-
 module.exports = function (Actionformation) {
+  Actionformation.computeNiveauQualificationEntree = (action) => {
+    return niveauQualificationEntreeFrom(action.niveauQualificationSortie)
+  }
+}
 
+function niveauQualificationEntreeFrom (niveauQualificationSortie) {
+  return {
+    'Pas de niveau': 'sans-diplome',
+    'Niveau V - Formations équivalentes aux CAP-BEP': 'sans-diplome',
+    'Niveau IV - Formations équivalentes à Bac ou Bac+1': 'cap-bep',
+    'Niveau III - Formations équivalentes à Bac+2': 'bac-bac+1',
+    'Niveau II - Formations équivalentes à Bac+3 ou Bac+4': 'bac+2'
+  }[niveauQualificationSortie] || 'sans-diplome'
 }
