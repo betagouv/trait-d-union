baa18585fdba6eb842251f5057f046a311e66704
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-03 14:34:07 +0200

    [Front] Filter by niveau now updates the results

--- front/app/components/offres-filter.js
+++ front/app/components/offres-filter.js
@@ -7,16 +7,27 @@ export default Component.extend({
   init () {
     this._super(...arguments)
     this.set('results', this.offres)
+    this.set('currentFilter', '')
   },
 
   actions: {
     filterByKeyword () {
       const offres = this.get('offres')
-      console.log(`${offres.length} offres to filter`)
-      console.log(`Will filter with ${this.keyword}`)
       const filteredOffres = filterByValue(offres, this.keyword)
-      console.log(filteredOffres.length)
       this.set('results', filteredOffres)
+    },
+
+    filterByNiveau: function (niveau) {
+      this.set('currentFilter', niveau)
+      const offres = this.get('offres')
+      if (!niveau) {
+        this.set('results', offres)
+        return
+      }
+      const results = offres.filter(offre => {
+        return offre.sessions[0].action.niveauQualificationEntree === niveau
+      })
+      this.set('results', results)
     }
   }
 })
