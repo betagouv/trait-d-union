6e15944ee9dc2db694b72f15daba725136c6c44f
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-04 14:44:06 +0200

    [front] Use an ENV VAR for the API URL

--- front/app/adapters/application.js
+++ front/app/adapters/application.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data'
 import { computed } from '@ember/object'
+import ENV from '../config/environment'
 
 export default DS.RESTAdapter.extend({
-  host: 'https://api.traitdunion.beta.gouv.fr',
-  // host: 'http://localhost:3000',
+  host: ENV.APP.apiUrl,
   namespace: 'api/v0',
   headers: computed(function () {
     return {
--- front/config/environment.js
+++ front/config/environment.js
@@ -18,8 +18,7 @@ module.exports = function (environment) {
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+      apiUrl: process.env.API_URL || 'okok' // https://api.traitdunion.beta.gouv.fr'
     },
 
     googleFonts: [
