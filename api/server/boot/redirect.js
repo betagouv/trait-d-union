2c3ea1b5ae55b6771f349c2a31316265e4cfac93
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-04 13:36:19 +0200

    [Redirect] Set up a redirect from root domain to candidat subdomain even on staging environment

--- api/server/boot/redirect.js
+++ api/server/boot/redirect.js
@@ -2,11 +2,12 @@ module.exports = (app) => {
   app.get('/', (req, res, next) => {
     const utmSource = req.query.utm_source
     const queryParams = utmSource ? `?utm_source=${utmSource}` : ''
-    if (isStaging(req) || isLocal(req)) {
+    if (isLocal(req)) {
       return next()
     }
+    const environment = isStaging(req) ? 'staging.' : ''
     res.writeHead(302, {
-      'Location': `http://candidat.traitdunion.beta.gouv.fr${queryParams}`
+      'Location': `https://${environment}candidat.traitdunion.beta.gouv.fr${queryParams}`
     })
     res.end()
   })
