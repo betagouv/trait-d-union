575fe3eeaa08969b80999f931055d7e40e0f77e8
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-11 09:15:08 +0200

    [tech] fix launching front app in dev mode

--- front/launch-server.js
+++ front/launch-server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const defaultPortNumber = 4200
+
 const staticDirectoryName = process.env.STATIC_DIR || 'dist'
 const serverPath = path.join(__dirname, '/', staticDirectoryName)
 app.use(express.static(serverPath))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || defaultPortNumber
 app.listen(port, () => console.log('Listening on', port))
