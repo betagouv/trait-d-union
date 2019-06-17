e928f063e5197a60d642a085e984e98cebb51e0f
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-17 11:49:29 +0200

    [Candidature] Add some logs

--- api/server/models/candidat.js
+++ api/server/models/candidat.js
@@ -1,15 +1,17 @@
 const { contactsApiClient, smtpApiClient } = require('../infrastructure/sendinblue-api-client')
 const sendSlackNotification = require('../infrastructure/send-slack-notification')
-const { error } = require('../infrastructure/logger')
+const { info, debug, error } = require('../infrastructure/logger')
 
 const createCandidatFromFormResponse = require('../features/candidats/create-candidat-from-form-response')
 const subscribeCandidateToMailingContactLists = require('../features/candidats/subscribe-candidate-to-mailing-contact-lists')
 const sendCandidatureEmail = require('../features/candidats/send-candidature-email')
-
 module.exports = function (Candidat) {
   Candidat.formResponse = async (data) => {
+    info(`New candidature received from Typeform`)
     const candidat = await createCandidatFromFormResponse({ Candidat }, data)
+    debug(`Created candidat: ${candidat.id}`)
     await subscribeCandidateToMailingContactLists({ contactsApiClient }, candidat)
+    debug(`Candidat added to SendInBlue contacts: ${candidat.id}`)
     const offreId = data.hidden.id_offre
     const offreFromDB = await Candidat.app.models.Offre.findById(offreId)
     if (offreFromDB) {
@@ -21,8 +23,10 @@ module.exports = function (Candidat) {
           notifyCandidatureFailure({ sendSlackNotification }, { candidat, offre, err })
           throw err
         })
+      info('Candidature successfully sent!')
       notifyCandidatureSuccess({ sendSlackNotification }, { candidat, offre })
     } else {
+      debug('No offre found - notifying it to Slack and exiting')
       notifyCandidatureFailure({ sendSlackNotification }, {
         candidat,
         offre: { id: offreId },
