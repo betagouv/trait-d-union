8b0ab0d7dad5b97c2848ba3d8bee5d6435360c20
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-18 11:00:04 +0200

    [front] serve index for every routes

--- front/launch-server.js
+++ front/launch-server.js
@@ -8,5 +8,7 @@ const staticDirectoryName = process.env.STATIC_DIR || 'dist'
 const serverPath = path.join(__dirname, '/', staticDirectoryName)
 app.use(express.static(serverPath))
 
+app.get('*', (req, res) => res.sendFile(path.join(serverPath, '/index.html')))
+
 const port = process.env.PORT || defaultPortNumber
 app.listen(port, () => console.log('Listening on', port))
