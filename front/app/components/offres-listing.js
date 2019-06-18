dfb29c7d6e7833f671bfc055ec3bf2acae842caf
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-18 17:54:42 +0200

    [Candidature] Embed typeform in a popup

--- front/app/components/offres-listing.js
+++ front/app/components/offres-listing.js
@@ -3,6 +3,7 @@ import ENV from '../config/environment'
 import Component from '@ember/component'
 import { storageFor } from 'ember-local-storage'
 import { inject as service } from '@ember/service'
+import typeformEmbed from '@typeform/embed'
 
 export default Component.extend({
   api: service(),
@@ -14,19 +15,26 @@ export default Component.extend({
       const userId = this.get('user').get('id')
       if (!userId) {
         this.get('user').set('id', uuidv4())
-        openTypeform(offre.id, this.get('user').get('id'))
+        openTypeform(offre, this.get('user').get('id'))
       } else {
         try {
           await this.get('api').applyToOffre(offre.id, userId)
         } catch (error) {
-          openTypeform(offre.id, userId)
+          openTypeform(offre, userId)
         }
-        offre.set('candidatureStatus', 'applied-offre')
       }
     }
   }
 })
 
-function openTypeform (offreId, userId) {
-  window.open(`${ENV.APP.typeformUrl}?id_offre=${offreId}&id_user=${userId}`, '_blank')
+function openTypeform (offre, userId) {
+  typeformEmbed.makePopup(`${ENV.APP.typeformUrl}?id_offre=${offre.id}&id_user=${userId}`, {
+    mode: 'popup',
+    autoOpen: true,
+    autoClose: 3,
+    hideScrollbars: true,
+    onSubmit: function () {
+      offre.set('candidatureStatus', 'applied-offre')
+    }
+  })
 }
