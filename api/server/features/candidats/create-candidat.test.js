8a5ddfb01fea985b5e602c465e95bf2add63a708
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-06-20 10:24:44 +0200

    [Candidature] Ensure candidat can be created even when typeform does not send a valid user_id

--- api/server/features/candidats/create-candidat.test.js
+++ api/server/features/candidats/create-candidat.test.js
@@ -18,4 +18,28 @@ describe('Create Candidat from form response (typeform webhook)', () => {
 
     expect(Candidat.create).to.have.been.calledWith(candidat)
   })
+
+  context('when candidat id is empty', () => {
+    const candidat = {
+      id: '',
+      email: 'an_account@example.com',
+      nomPrenom: 'Lorem ipsum dolor Nom Prenom',
+      telephone: 'Lorem ipsum dolor Phone',
+      cvUrl: 'https://admin.typeform.com/form/P6NFOZ/field/WlLbkyygWkkh/results/file.ext/download'
+    }
+    const Candidat = {
+      create: sinon.spy(async () => ({}))
+    }
+
+    it('creates Candidat in DB', async () => {
+      await createCandidat({ Candidat }, candidat)
+
+      expect(Candidat.create).to.have.been.calledWith({
+        email: candidat.email,
+        nomPrenom: candidat.nomPrenom,
+        telephone: candidat.telephone,
+        cvUrl: candidat.cvUrl
+      })
+    })
+  })
 })
