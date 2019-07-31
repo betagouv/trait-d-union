f712ed3f28e724fd13fb00b9997239ae812470e3
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-07-31 15:04:41 +0200

    [Sending candidature] add some debug logs

--- api/server/features/candidature/send-candidature-email.js
+++ api/server/features/candidature/send-candidature-email.js
@@ -1,4 +1,4 @@
-const { error } = require('../../infrastructure/logger')
+const { error, debug } = require('../../infrastructure/logger')
 const { DateTime } = require('luxon')
 
 module.exports = ({ smtpApiClient }) => async ({ offre, candidat, candidatureId, retry = false }) => {
@@ -7,27 +7,14 @@ module.exports = ({ smtpApiClient }) => async ({ offre, candidat, candidatureId,
   const attachment = destinataireIsPoleEmploi(offre) ? [PNSMPattachment, { url: cvUrl }] : [PNSMPattachment]
   const tag = retry ? 'Relance_Candidature_DE' : 'Candidature_DE'
   const tags = [`${tag}${process.env.APP_ENV === 'staging' ? '_Staging' : ''}`]
-  const { messageId } = await smtpApiClient.sendTransacEmail({
-    templateId,
-    to: [{ name: offre.contact.nom, email: offre.contact.courriel }],
-    replyTo: { name: candidat.nomPrenom, email: candidat.email },
-    params: {
-      Titre_offre: offre.intitule,
-      id_offre: offre.id,
-      Nom_prenom: candidat.nomPrenom,
-      URL_CV: cvUrl,
-      Age: candidat.age,
-      Telephone: candidat.telephone,
-      id_candidature: candidatureId,
-      email_candidat: candidat.email,
-      poleEmploiId: candidat.poleEmploiId,
-      sessionDateDebut: formatDate(offre.sessions[0].dateDebut),
-      sessionDateFin: formatDate(offre.sessions[0].dateFin),
-      sessionDuree: offre.sessions[0].duration
-    },
-    attachment,
-    tags
-  }).catch(err => {
+
+  const emailParameters = getEmailParameters(templateId, offre, candidat, cvUrl, candidatureId, attachment, tags)
+
+  debug(`Sending email candidature with params: ${JSON.stringify(emailParameters)}`)
+  if (process.env.TT_FF_DO_NOT_SEND_EMAIL === 'true') {
+    return 'message_id'
+  }
+  const { messageId } = await smtpApiClient.sendTransacEmail(emailParameters).catch(err => {
     error(`Error while sending candidature email with SendInBlue - ${err.response ? err.response.text : err}`)
     throw err
   })
@@ -63,6 +50,31 @@ const templateIds = {
 const PNSMPattachment = { url: 'https://labonneformation.pole-emploi.fr/pdf/cerfa_13912-04.pdf' }
 
 function formatDate (date) {
+  debug(`Formatting date ${date}`)
   const dateTime = DateTime.fromISO(date)
   return dateTime.setLocale('fr').toLocaleString(DateTime.DATE_FULL)
+}
+
+function getEmailParameters (templateId, offre, candidat, cvUrl, candidatureId, attachment, tags) {
+  return {
+    templateId,
+    to: [{ name: offre.contact.nom, email: offre.contact.courriel }],
+    replyTo: { name: candidat.nomPrenom, email: candidat.email },
+    params: {
+      Titre_offre: offre.intitule,
+      id_offre: offre.id,
+      Nom_prenom: candidat.nomPrenom,
+      URL_CV: cvUrl,
+      Age: candidat.age,
+      Telephone: candidat.telephone,
+      id_candidature: candidatureId,
+      email_candidat: candidat.email,
+      poleEmploiId: candidat.poleEmploiId,
+      sessionDateDebut: formatDate(offre.sessions[0].dateDebut),
+      sessionDateFin: formatDate(offre.sessions[0].dateFin),
+      sessionDuree: offre.sessions[0].duration
+    },
+    attachment,
+    tags
+  }
 }
