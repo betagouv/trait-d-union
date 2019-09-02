04a7d397c01d68559b6f61e279816535a10f775b
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-09-02 09:19:21 +0200

    [Retrieve user] as ilike does not exist on Mongo, use like with 'i' option

--- api/server/features/candidats/forbid-find-candidat.js
+++ api/server/features/candidats/forbid-find-candidat.js
@@ -1,3 +1,3 @@
 module.exports = (args) => {
-  return !args || !args.filter || !args.filter.where || !args.filter.where.email || !args.filter.where.email.ilike
+  return !args || !args.filter || !args.filter.where || !args.filter.where.email || !args.filter.where.email.like
 }
