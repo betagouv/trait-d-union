b84048bc74ce259579cca4b156e53dc41b2b9513
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-11-13 07:56:50 +0100

    Use latest version of envie so configuration can be easily read from env var

--- api/server.js
+++ api/server.js
@@ -3,13 +3,13 @@ const Inert = require('@hapi/inert')
 const Vision = require('@hapi/vision')
 const HapiSwagger = require('hapi-swagger')
 const Pack = require('./package')
-// const configurationService = require('./src/services/configuration-service')
+const configurationService = require('./src/services/configuration-service')
 const routes = require('./src/routes')
 
 exports.createServer = async () => {
   const serverInstance = new Hapi.Server({
-    port: process.env.PORT || 8080, // configurationService.get('PORT'),
-    host: process.env.HOST || '0.0.0.0'
+    port: configurationService.get('PORT'),
+    host: configurationService.get('HOST')
   })
   serverInstance.event('onPreStart', () => {
   })
