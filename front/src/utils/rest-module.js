3ffa95cda981833281f5326d53387d13adc9a38f
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-11-19 09:48:33 +0100

    Add some log to debug API URL

--- front/src/utils/rest-module.js
+++ front/src/utils/rest-module.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL })
 
-export default axiosInstance
+console.info(`REACT_APP_API_BASE_URL = ${process.env.REACT_APP_API_BASE_URL}`)
 
+export default axiosInstance
