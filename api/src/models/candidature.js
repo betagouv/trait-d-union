6ece9a3e72d38a4224e39df073307654caffa2d3
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-11-28 16:14:07 +0100

    Add slack notification when candidature is received

--- api/src/models/candidature.js
+++ api/src/models/candidature.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const databaseService = require('../services/database-service')
 const niveauxEtude = require('./enums/niveaux-etude')
+const sendSlackNotification = require('../infrastructure/send-slack-notification')
 
 const Candidature = databaseService.define('candidature', {
   id: {
@@ -57,5 +58,10 @@ const Candidature = databaseService.define('candidature', {
 Candidature.associate = ({ Offre }) => {
   Candidature.belongsTo(Offre)
 }
+
+Candidature.afterCreate(async (candidature, options) => {
+  const candidatureFromDB = await Candidature.findByPk(candidature.id, { include: 'offre' })
+  return sendSlackNotification({ text: `:mailbox_with_mail: 1 nouvelle candidature vient d'être déposée par ${candidatureFromDB.firstName} ${candidatureFromDB.lastName} : \n [Offre \`${candidatureFromDB.offre.id}\`] job de ${candidatureFromDB.offre.jobTitle} situé à ${candidatureFromDB.offre.address}` })
+})
 
 module.exports = Candidature
