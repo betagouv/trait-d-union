9ae33bd09387c93dd0f37dccde00380b59072c0d
Guillaume LAGORCE <guillaume.lagorce@beta.gouv.fr>
2019-12-17 16:58:27 +0100

    use SSL for iframe integratin

--- front/src/components/Stats.js
+++ front/src/components/Stats.js
@@ -4,7 +4,7 @@ import IframeResizer from 'iframe-resizer-react'
 const Stats = () => {
   return (
     <IframeResizer
-      src="http://metabase.traitdunion.beta.gouv.fr/public/dashboard/804877c6-a815-4061-8858-feffda560130"
+      src="https://metabase.traitdunion.beta.gouv.fr/public/dashboard/804877c6-a815-4061-8858-feffda560130"
       style={{ height: '1700px', minHeight: '100%', width: '1px', minWidth: '100%' }}
     />
   )
